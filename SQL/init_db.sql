1dc5c94a09613b81ee672a2804fce9adb5fe91dc
lance.yang <lance.yang@zyt.com>
2024-11-14 15:34:35 +0800

    关注服务添加唯一联合索引保证幂等性+消费者数据落库逻辑

--- SQL/init_db.sql
+++ SQL/init_db.sql
@@ -136,15 +136,17 @@ CREATE TABLE `t_following` (
                                `user_id` bigint unsigned NOT NULL COMMENT '用户ID',
                                `following_user_id` bigint unsigned NOT NULL COMMENT '关注的用户ID',
                                `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-                               PRIMARY KEY (`id`) USING BTREE,
-                               KEY `idx_user_id` (`user_id`)
+                               PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户关注表';
+
+ALTER TABLE t_following ADD UNIQUE uk_user_id_following_user_id(user_id, following_user_id);
 
 CREATE TABLE `t_fans` (
                           `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
                           `user_id` bigint unsigned NOT NULL COMMENT '用户ID',
                           `fans_user_id` bigint unsigned NOT NULL COMMENT '粉丝的用户ID',
                           `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-                          PRIMARY KEY (`id`) USING BTREE,
-                          KEY `idx_user_id` (`user_id`)
+                          PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户粉丝表';
+
+ALTER TABLE t_fans ADD UNIQUE uk_user_id_fans_user_id(user_id, fans_user_id);
--- linker-user-relation/linker-user-relation-biz/src/main/java/com/aus/linker/user/relation/biz/consumer/FollowUnfollowConsumer.java
+++ linker-user-relation/linker-user-relation-biz/src/main/java/com/aus/linker/user/relation/biz/consumer/FollowUnfollowConsumer.java
@@ -1,11 +1,22 @@
 package com.aus.linker.user.relation.biz.consumer;
 
+import com.aus.framework.common.utils.JsonUtil;
 import com.aus.linker.user.relation.biz.constant.MQConstants;
+import com.aus.linker.user.relation.biz.domain.dataobject.FansDO;
+import com.aus.linker.user.relation.biz.domain.dataobject.FollowingDO;
+import com.aus.linker.user.relation.biz.domain.service.FansService;
+import com.aus.linker.user.relation.biz.domain.service.FollowingService;
+import com.aus.linker.user.relation.biz.model.dto.FollowUserMqDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Component
 @Slf4j
@@ -13,6 +24,15 @@ import org.springframework.stereotype.Component;
         topic = MQConstants.TOPIC_FOLLOW_OR_UNFOLLOW        // 消费的 Topic 主题
 )
 public class FollowUnfollowConsumer implements RocketMQListener<Message> {
+
+    @Resource
+    private FollowingService followingService;
+
+    @Resource
+    private FansService fansService;
+
+    @Resource
+    private TransactionTemplate transactionTemplate;
 
     @Override
     public void onMessage(Message message) {
@@ -22,6 +42,64 @@ public class FollowUnfollowConsumer implements RocketMQListener<Message> {
         String tags = message.getTags();
 
         log.info("==> FollowUnfollowConsumer 消费了消息 {}, tags: {}", bodyJsonStr, tags);
+
+        // 根据 MQ 标签，判断操作类型
+        if (Objects.equals(tags, MQConstants.TAG_FOLLOW)) { // 关注
+            handleFollowTagMessage(bodyJsonStr);
+        } else if (Objects.equals(tags, MQConstants.TAG_UNFOLLOW)) { // 取关
+            // TODO
+        }
+
+    }
+
+    /**
+     * 关注
+     * @param bodyJsonStr
+     */
+    private void handleFollowTagMessage(String bodyJsonStr) {
+        // 将消息体 Json 字符串转为 DTO 对象
+        FollowUserMqDTO followUserMqDTO = JsonUtil.parseObject(bodyJsonStr, FollowUserMqDTO.class);
+
+        // 判空
+        if (Objects.isNull(followUserMqDTO)) return;
+
+        // 幂等性：通过联合唯一索引保证
+
+        Long userId = followUserMqDTO.getUserId();
+        Long followUserId = followUserMqDTO.getFollowUserId();
+        LocalDateTime createTime = followUserMqDTO.getCreateTime();
+
+        // 编程式提交事务
+        boolean isSuccess = Boolean.TRUE.equals(transactionTemplate.execute(status -> {
+            try {
+                // 关注成功需往数据库添加两条记录
+                // 关注表
+                boolean result = followingService.save(FollowingDO.builder()
+                        .userId(userId)
+                        .followingUserId(followUserId)
+                        .createTime(createTime)
+                        .build()
+                );
+                // 粉丝表
+                if (result) {
+                    result = fansService.save(FansDO.builder()
+                            .userId(followUserId)
+                            .fansUserId(userId)
+                            .createTime(createTime)
+                            .build()
+                    );
+                }
+                return result;
+            } catch (Exception e) {
+                status.setRollbackOnly();   // 标记事务为回滚
+                log.error("", e);
+            }
+            return false;
+        }));
+
+        log.info("## 数据库添加记录结果：{}", isSuccess);
+        // TODO: 更新 Redis 中被关注用户的 ZSet 粉丝列表
+
     }
 
 }
