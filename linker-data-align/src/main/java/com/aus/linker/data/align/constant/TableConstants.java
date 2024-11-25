f3f71c215a4d25c920c6a2ccc318223be257a1b9
lance.yang <lance.yang@zyt.com>
2024-11-25 19:06:34 +0800

    XXL定时任务：预创建7base*3分片日增量数据库表

---
+++ linker-data-align/src/main/java/com/aus/linker/data/align/constant/TableConstants.java
@@ -0,0 +1,15 @@
+package com.aus.linker.data.align.constant;
+
+public class TableConstants {
+
+    /**
+     * 表名中的分隔符
+     */
+    private static final String TABLE_NAME_SEPARATOR = "_";
+
+    public static String buildTableNameSuffix(String date, int hashKey) {
+        // 拼接完整的表名
+        return date + TABLE_NAME_SEPARATOR + hashKey;
+    }
+
+}
